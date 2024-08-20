0a568a4aae9d15d25b66fdbd22bb730a3d1916bc
Iw Orxs <iworxs@gmail.com>
2024-08-20 09:31:18 +0100

    UserMedia images for component card

--- src/components/UserMediaCard.tsx
+++ src/components/UserMediaCard.tsx
@@ -1,6 +1,7 @@
 //src/components/UserMediaCard.tsx
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 const UserMediaCard = ({ userId }: { userId: string }) => {
   return (
@@ -12,6 +13,73 @@ const UserMediaCard = ({ userId }: { userId: string }) => {
           <Link href='/' className='text-blue-500 text-sm'>
             See all
           </Link>
+        </div>
+        {/* BOTTOM SECTION IMAGES*/}
+        <div className='flex gap-4 justify-between flex-wrap'>
+          <div className='relative w-1/5 h-24'>
+            <Image
+              src='https://images.pexels.com/photos/3225517/pexels-photo-3225517.jpeg?auto=compress&cs=tinysrgb&w=600'
+              alt='nature-1'
+              fill
+              className='rounded-md object-cover'
+            />
+          </div>
+          <div className='relative w-1/5 h-24'>
+            <Image
+              src='https://images.pexels.com/photos/15286/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=600'
+              alt='nature-2'
+              fill
+              className='rounded-md object-cover'
+            />
+          </div>
+          <div className='relative w-1/5 h-24'>
+            <Image
+              src='https://images.pexels.com/photos/707344/pexels-photo-707344.jpeg?auto=compress&cs=tinysrgb&w=600'
+              alt='nature-3'
+              fill
+              className='rounded-md object-cover'
+            />
+          </div>
+          <div className='relative w-1/5 h-24'>
+            <Image
+              src='https://images.pexels.com/photos/3408744/pexels-photo-3408744.jpeg?auto=compress&cs=tinysrgb&w=600'
+              alt='nature-4'
+              fill
+              className='rounded-md object-cover'
+            />
+          </div>
+          <div className='relative w-1/5 h-24'>
+            <Image
+              src='https://images.pexels.com/photos/1820563/pexels-photo-1820563.jpeg?auto=compress&cs=tinysrgb&w=600'
+              alt='nature-5'
+              fill
+              className='rounded-md object-cover'
+            />
+          </div>
+          <div className='relative w-1/5 h-24'>
+            <Image
+              src='https://images.pexels.com/photos/1424239/pexels-photo-1424239.jpeg?auto=compress&cs=tinysrgb&w=600'
+              alt='nature-6'
+              fill
+              className='rounded-md object-cover'
+            />
+          </div>
+          <div className='relative w-1/5 h-24'>
+            <Image
+              src='https://images.pexels.com/photos/237321/pexels-photo-237321.jpeg?auto=compress&cs=tinysrgb&w=600'
+              alt='nature-7'
+              fill
+              className='rounded-md object-cover'
+            />
+          </div>
+          <div className='relative w-1/5 h-24'>
+            <Image
+              src='https://images.pexels.com/photos/417083/pexels-photo-417083.jpeg?auto=compress&cs=tinysrgb&w=600'
+              alt='nature-8'
+              fill
+              className='rounded-md object-cover'
+            />
+          </div>
         </div>
       </div>
     </>
