31db29507c299f3976fe3be66556e47ae85f9e29
Iw Orxs <iworxs@gmail.com>
2024-08-20 09:53:20 +0100

    Left section items

---
+++ src/components/ProfileCard.tsx
@@ -0,0 +1,12 @@
+//src/components/ProfileCard.tsx
+import React from "react";
+
+const ProfileCard = () => {
+  return (
+    <>
+      <div>ProfileCard</div>
+    </>
+  );
+};
+
+export default ProfileCard;
