5c27d9965875aa5c56537bd9d10d6fea9eb2c066
Iw Orxs <iworxs@gmail.com>
2024-08-24 16:25:21 +0100

    updated loading speed of component

--- src/components/RightMenu.tsx
+++ src/components/RightMenu.tsx
@@ -1,19 +1,25 @@
 //src/component/RightMenu.tsx
-import React from "react";
+import React, { Suspense } from "react";
 import Advert from "./Advert";
 import Birthdays from "./Birthdays";
 import FriendRequest from "./FriendRequest";
 import UserInfoCard from "./UserInfoCard";
 import UserMediaCard from "./UserMediaCard";
+import { User } from "@prisma/client";
 
-const RightMenu = ({ userId }: { userId?: string }) => {
+// const RightMenu = ({ userId }: { userId?: string }) => {};
+const RightMenu = ({ user }: { user?: User }) => {
   return (
     <>
       <div className='flex flex-col gap-6'>
-        {userId ? (
+        {user ? (
           <>
-            <UserInfoCard userId={userId} />
-            <UserMediaCard userId={userId} />
+            <Suspense fallback='loading...'>
+              <UserInfoCard user={user} />
+            </Suspense>
+            <Suspense fallback='loading...'>
+              <UserMediaCard user={user} />
+            </Suspense>
           </>
         ) : null}
         <FriendRequest />
