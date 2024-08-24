04ee2ab1042c12341a47a6d51024b0f6c93f58f4
Iw Orxs <iworxs@gmail.com>
2024-08-24 12:32:11 +0100

    updates Aug 23

--- src/components/AddPost.tsx
+++ src/components/AddPost.tsx
@@ -1,8 +1,33 @@
 //src/components/AddPost.tsx
 import React from "react";
+import prisma from "@/lib/client";
+import { auth } from "@clerk/nextjs/server";
 import Image from "next/image";
 
 const AddPost = () => {
+  // userId from Clerk Auth client
+  const { userId } = auth();
+  console.log(userId);
+
+  // request to add a Post form action= testAction
+  // const testAction = async (formData: FormData) => {
+  //   // this is a Server Action
+  //   "use server";
+  //   if (!userId) return;
+
+  //   const descPost = formData.get("descPost") as string;
+  //   try {
+  //     const res = await prisma.post.create({
+  //       data: {
+  //         userId: userId,
+  //         descPost: descPost,
+  //       },
+  //     });
+  //     console.log(res);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
   return (
     <>
       <div className='p-4 bg-white shadow-md rounded-lg flex gap-4 justify-between text-sm'>
@@ -16,10 +41,10 @@ const AddPost = () => {
         />
         {/* POST */}
         <div className='flex-1 border-2 border-solid border-red-600'>
-          {/* TEXT INPUT */}
-          <div className='flex gap-4'>
+          {/* TEXT INPUT via a Form*/}
+          <form action='' className='flex gap-4'>
             <textarea
-              name=''
+              name='descPost'
               id=''
               placeholder='Your thoughts'
               className='flex-1 bg-slate-100 rounded-lg p-2'></textarea>
@@ -30,7 +55,8 @@ const AddPost = () => {
               height={20}
               className='w-5 h-5 cursor-pointer self-end'
             />
-          </div>
+            <button>Send Post</button>
+          </form>
           {/* POST OPTIONS */}
           <div className='flex items-center gap-4 mt-4 text-gray-400 flex-wrap'>
             <div className='flex items-center gap-2 cursor-pointer'>
